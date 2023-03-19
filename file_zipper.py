88fa00ade9f9f24d7a0ae1f238e61d5f0c224722
Abinash Dutta <36304692+abinash54@users.noreply.github.com>
2023-03-19 18:51:36 +0530

    Update and rename zipper.py to file_zipper.py

--- file_zipper.py
+++ file_zipper.py
@@ -1,13 +1,18 @@
+"""
+this program can be used for zipping multiple files in a location, 
+for your personal use modify the variables as mentioned as your personal need and run it.
+"""
+
 import os
 from zipfile import ZipFile
 import argparse, sys
 
 if __name__=='__main__':
         
-    srcLocation = './test_files'
-    fileTypes = ['mp3', 'mp4']
+    srcLocation = './test_files' # this is where program looks for files to be zipped
+    fileTypes = ['mp3', 'mp4'] #this will zip mp3 and mp4 files separately in a zip
     outputLocation ='./op'
-    batchSize = 3
+    batchSize = 3 # fixed number of batchsize which dictates number of files in each zip
     # scanning the directory path
     count = 1
     try:
